0a9e98e7717895fad43a9e780df05922e3c0a4cf
Motika Artem <artmotika@gmail.com>
2022-11-03 04:20:24 +0300

    .

--- project/__main__.py
+++ project/__main__.py
@@ -1,38 +1,36 @@
 from pyformlang.cfg import CFG
-from project.extended_context_free_grammar import ECFG
+from project.ecfg import ECFG
 from project.rsm import RSM
+from pyformlang.regular_expression.regex import Regex
+from networkx.algorithms.isomorphism.isomorph import is_isomorphic
+from networkx.algorithms.isomorphism.matchhelpers import categorical_multiedge_match
+from networkx.algorithms.isomorphism.matchhelpers import categorical_node_match
 
 print("exec sources directory")
 cfg = CFG.from_text(
     """
-    S -> NP VP PUNC
-    PUNC -> . | !
-    VP -> V NP
-    V -> buys | touches | sees
-    NP -> georges | jacques | leo | Det N
-    Det -> a | an | the
-    N -> gorilla | dog | carrots"""
+    S -> NP VP
+    NP -> georges | Det N
+    Det -> a | the
+    N -> carrots"""
 )
 
-for p in cfg.productions:
-    print("---------")
-    print(p.head)
-    print(p.body)
-    print("---------")
-
+# for p in cfg.productions:
+#     print("---------")
+#     print(p.head)
+#     print(p.body)
+#     print("---------")
+#
+# print(Regex("12 2 3 "))
 ecfg = ECFG.from_pyfl_cfg(cfg)
 productions = ecfg.productions
 for ph in productions.keys():
-    body = ""
-    for pb in productions.get(ph):
-        body += str(pb) + " | "
-    print(f"\n{ph} -> {body}\n")
+    print(f"\n{ph} -> {productions.get(ph)}\n")
 
 print("---------")
 set1 = set()
-set2 = set()
 
-rsm = ecfg.to_rsm()
+rsm = RSM.from_ecfg(ecfg)
 
 print(rsm.start_module)
 modules = rsm.modules
@@ -44,25 +42,10 @@ for k in modules.keys():
 
 print("--------")
 
-rsm = RSM.from_pyfl_cfg(cfg)
+print("-------------")
+for info in set1:
+    print(info)
+print("-------------")
 
-print(rsm.start_module)
-modules = rsm.modules
-for k in modules.keys():
-    for x, y, z in modules.get(k):
-        set2.add(
-            f"\n{k}:   {x} - {y} -> {z}  start_states = {modules.get(k).start_states} final_states = {modules.get(k).final_states}\n"
-        )
-
-print(set1 == set2)
-
-# ecfg = ECFG.from_pyfl_cfg(cfg)
-# print(ecfg.start_symbol)
-# print(ecfg.productions)
-# rsm = ecfg.to_rsm()
-
-
-# rsm = RSM.from_pyfl_cfg(cfg)
-
-
-# print(cfg.get_generating_symbols())
+for p in Regex("(a|b)* c").to_cfg().productions:
+    print(p)
--- project/rsm.py
+++ project/rsm.py
@@ -1,8 +1,10 @@
 from collections import deque
 
-from pyformlang.cfg.cfg import CFG, Variable
+from pyformlang.cfg.cfg import CFG, Variable, Production
 from pyformlang.finite_automaton import EpsilonNFA
-from pyformlang.finite_automaton import State, Symbol
+from pyformlang.finite_automaton import State
+
+from project.ecfg import ECFG
 
 
 class RSM:
@@ -15,27 +17,33 @@ class RSM:
         self.modules = modules
         self.stack = deque[tuple[Variable, State]]()
 
-    # def to_ecfg(self) -> ECFG:
-    #     return ECFG()
-    #
-    # def to_pyfl_cfg(self) -> CFG:
-    #     return CFG()
-
     @classmethod
-    def from_pyfl_cfg(cls, cfg) -> "RSM":
-        modules = {p.head: EpsilonNFA(start_state={State(0)}) for p in cfg.productions}
-        for p in cfg.productions:
-            nfa = modules.get(p.head)
-            body = p.body
-            body_length = len(body)
-            nfa.add_final_state(State(body_length))
-            for cfg_obj_ind in range(body_length):
-                nfa.add_transition(
-                    State(cfg_obj_ind),
-                    Symbol(body[cfg_obj_ind]),
-                    State(cfg_obj_ind + 1),
-                )
-        return cls(start_module=cfg.start_symbol, modules=modules)
+    def from_ecfg(cls, ecfg) -> "RSM":
+        modules = {}
+        productions = ecfg.productions
+        for var in productions.keys():
+            production = productions.get(var)
+            if production is not None:
+                modules[var] = production.to_epsilon_nfa().minimize()
+        return cls(start_module=ecfg.start_symbol, modules=modules)
+
+    def to_ecfg(self) -> ECFG:
+        productions = {}
+        modules = self.modules
+        for var in modules.keys():
+            productions[var] = modules.get(var).to_regex()
+        return ECFG(start_symbol=self.start_module, productions=productions)
+
+    def to_pyfl_cfg(self) -> CFG:
+        modules = self.modules
+        res_cfg = CFG(start_symbol=self.start_module, productions=set())
+        for var in modules.keys():
+            temp_cfg = modules.get(var).to_regex().to_cfg(starting_symbol=f"{var}S")
+            new_cfg = res_cfg.union(temp_cfg)
+            productions = set(new_cfg.productions)
+            productions.add(Production(head=var, body=[Variable(f"{var}S")]))
+            res_cfg = CFG(start_symbol=self.start_module, productions=productions)
+        return res_cfg
 
     def minimize(self) -> "RSM":
         new_modules = {
