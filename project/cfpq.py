4c5b878f0de5911e29b9d5cdde1d1b8c9c444f89
Motika Artem <artmotika@gmail.com>
2022-11-17 01:07:46 +0300

    .

--- project/cfpq.py
+++ project/cfpq.py
@@ -17,12 +17,12 @@ def cfpq(
     if final_nodes is None:
         final_nodes = graph.nodes
     for (e1, e2, e3) in helings(cfg, graph):
-        if e2 in start_nodes and e3 in final_nodes and e1 == cfg.start_symbol:
-            result.add((e2, e3))
+        if e1 in start_nodes and e3 in final_nodes and e2 == cfg.start_symbol:
+            result.add((e1, e3))
     return result
 
 
-def helings(cfg, graph) -> set[tuple[Variable, Any, Any]]:
+def helings(cfg, graph) -> set[tuple[Any, Variable, Any]]:
     cfg = cfg_to_weakened_form_chomsky(cfg)
     epsilon_var_heads = set()
     one_var_productions = set()
@@ -41,37 +41,37 @@ def helings(cfg, graph) -> set[tuple[Variable, Any, Any]]:
             raise ValueError("Cfg in helings() is not in weakened form Chomsky")
 
     one_var = {
-        (p.head, node1, node2)
+        (node1, p.head, node2)
         for (node1, node2, symbol) in graph.edges(data=True)
         for p in one_var_productions
         if symbol["label"] == p.body[0].value
     }
-    eps_var = {(head, node, node) for head in epsilon_var_heads for node in graph.nodes}
+    eps_var = {(node, head, node) for head in epsilon_var_heads for node in graph.nodes}
 
     r = one_var | eps_var
     m = deque(r.copy())
     while m:
-        (Ni, v, u) = m.pop()
-        for (Nj, nv, v_old) in list(r):
+        (v, Ni, u) = m.pop()
+        for (nv, Nj, v_old) in list(r):
             if v_old == v:
                 for p in two_var_productions:
                     head = p.head
                     if (
                         Nj == p.body[0].value
                         and Ni == p.body[1].value
-                        and (head, nv, u) not in r
+                        and (nv, head, u) not in r
                     ):
-                        m.appendleft((head, nv, u))
-                        r.add((head, nv, u))
-        for (Nj, u_old, nv) in list(r):
+                        m.appendleft((nv, head, u))
+                        r.add((nv, head, u))
+        for (u_old, Nj, nv) in list(r):
             if u_old == u:
                 for p in two_var_productions:
                     head = p.head
                     if (
                         Ni == p.body[0].value
                         and Nj == p.body[1].value
-                        and (head, v, nv) not in r
+                        and (v, head, nv) not in r
                     ):
-                        m.appendleft((head, v, nv))
-                        r.add((head, v, nv))
+                        m.appendleft((v, head, nv))
+                        r.add((v, head, nv))
     return r
