ed373573ff910cc08e5685e07e8eaa0e53be501a
Motika Artem <artmotika@gmail.com>
2022-11-23 22:17:13 +0300

    Task 10

--- project/cfpq.py
+++ project/cfpq.py
@@ -3,6 +3,13 @@ from networkx.classes.multidigraph import MultiDiGraph
 from typing import Any
 from collections import deque
 from project.cfg_utilities import cfg_to_weakened_form_chomsky
+from enum import Enum
+from scipy.sparse import dok_matrix
+
+
+class CFPQAlgorithm(Enum):
+    HELINGS = 0
+    MATRIX = 1
 
 
 def cfpq(
@@ -10,35 +17,71 @@ def cfpq(
     graph: MultiDiGraph,
     start_nodes: set = None,
     final_nodes: set = None,
+    algo: CFPQAlgorithm = CFPQAlgorithm.HELINGS,
 ) -> set[tuple[Any, Any]]:
     result = set()
     if start_nodes is None:
         start_nodes = graph.nodes
     if final_nodes is None:
         final_nodes = graph.nodes
-    for (e1, e2, e3) in helings(cfg, graph):
-        if e1 in start_nodes and e3 in final_nodes and e2 == cfg.start_symbol:
+    for (e1, e2, e3) in [helings, matrix][algo.value](cfg, graph):
+        if e2 == cfg.start_symbol and e1 in start_nodes and e3 in final_nodes:
             result.add((e1, e3))
     return result
 
 
-def helings(cfg, graph) -> set[tuple[Any, Variable, Any]]:
+def matrix(cfg: CFG, graph: MultiDiGraph) -> set[tuple[Any, Variable, Any]]:
     cfg = cfg_to_weakened_form_chomsky(cfg)
-    epsilon_var_heads = set()
-    one_var_productions = set()
-    two_var_productions = set()
+    (
+        epsilon_var_heads,
+        one_var_productions,
+        two_var_productions,
+    ) = _get_weakened_form_productions(cfg)
 
-    for p in cfg.productions:
-        head = p.head
-        len_p_body = len(p.body)
-        if len_p_body == 0:
-            epsilon_var_heads.add(head)
-        elif len_p_body == 1:
-            one_var_productions.add(p)
-        elif len_p_body == 2:
-            two_var_productions.add(p)
-        else:
-            raise ValueError("Cfg in helings() is not in weakened form Chomsky")
+    n = len(graph.nodes)
+
+    bool_decomposed_graph_mtx = {
+        var: dok_matrix((n, n), dtype=bool) for var in cfg.variables
+    }
+
+    nodes = list(graph.nodes)
+    node_to_int = {node: idx for idx, node in enumerate(graph.nodes)}
+
+    for head in epsilon_var_heads:
+        for i in range(n):
+            bool_decomposed_graph_mtx[head][i, i] = True
+    for (node1, node2, symbol) in graph.edges(data=True):
+        for p in one_var_productions:
+            if symbol["label"] == p.body[0].value:
+                bool_decomposed_graph_mtx[p.head][
+                    node_to_int.get(node1), node_to_int.get(node2)
+                ] = True
+    for var in bool_decomposed_graph_mtx.keys():
+        bool_decomposed_graph_mtx[var] = bool_decomposed_graph_mtx[var].tocsr()
+    changed = True
+    while changed:
+        changed = False
+        for p in two_var_productions:
+            old_mtx_cnz = bool_decomposed_graph_mtx[p.head].count_nonzero()
+            bool_decomposed_graph_mtx[p.head] += (
+                bool_decomposed_graph_mtx[p.body[0]]
+                @ bool_decomposed_graph_mtx[p.body[1]]
+            )
+            changed |= old_mtx_cnz != bool_decomposed_graph_mtx[p.head].count_nonzero()
+    return {
+        (nodes[i], var, nodes[j])
+        for var in bool_decomposed_graph_mtx.keys()
+        for i, j in zip(*bool_decomposed_graph_mtx[var].nonzero())
+    }
+
+
+def helings(cfg: CFG, graph: MultiDiGraph) -> set[tuple[Any, Variable, Any]]:
+    cfg = cfg_to_weakened_form_chomsky(cfg)
+    (
+        epsilon_var_heads,
+        one_var_productions,
+        two_var_productions,
+    ) = _get_weakened_form_productions(cfg)
 
     one_var = {
         (node1, p.head, node2)
@@ -76,3 +119,21 @@ def helings(cfg, graph) -> set[tuple[Any, Variable, Any]]:
                         m.appendleft((v, head, nv))
                         r.add((v, head, nv))
     return r
+
+
+def _get_weakened_form_productions(cfg: CFG) -> tuple[set, set, set]:
+    epsilon_var_heads = set()
+    one_var_productions = set()
+    two_var_productions = set()
+    for p in cfg.productions:
+        head = p.head
+        len_p_body = len(p.body)
+        if len_p_body == 0:
+            epsilon_var_heads.add(head)
+        elif len_p_body == 1:
+            one_var_productions.add(p)
+        elif len_p_body == 2:
+            two_var_productions.add(p)
+        else:
+            raise ValueError("Cfg in helings() is not in weakened form Chomsky")
+    return (epsilon_var_heads, one_var_productions, two_var_productions)
